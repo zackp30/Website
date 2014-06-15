357a0725f77f6df0b87dfc08d1ae82153d970f46
Zack Piper <zack.piper123@gmail.com>
2014-06-15 15:35:31 +0100

    Fixed new page template, and also fixed Keybase proof.

--- config.rb
+++ config.rb
@@ -53,6 +53,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :markdown_engine, :kramdown
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
---
+++ source/stuff/index.md
@@ -0,0 +1,6 @@
+---
+title: Stuff
+---
+
+
+Stuff!
